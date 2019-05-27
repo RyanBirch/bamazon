aa9ac17dbdbc328d856e2d550ca14749d372b23f
Ryan Birch <ryan.birch@gmail.com>
2019-05-27 03:48:03 -0400

    completed create department function

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -23,7 +23,7 @@ function askSupervisor() {
         {
             type: 'list',
             message: 'Select an option',
-            choices: ['View Product Sales by Department', 'Create New Department', 'Exit'],
+            choices: ['View Product Sales by Department', 'View Departments', 'Create New Department', 'Exit'],
             name: 'choice'
         }
     ])
@@ -32,6 +32,8 @@ function askSupervisor() {
             viewProductSales()
         } else if (answer.choice === 'Create New Department') {
             createNewDepartment()
+        } else if (answer.choice === 'View Departments') {
+            viewDepartments()
         } else {
             connection.end()
             process.exit()
@@ -44,5 +46,49 @@ function viewProductSales() {
 }
 
 function createNewDepartment() {
-    
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department you would like to add?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is the overhead cost of the department?',
+            name: 'cost'
+        }
+    ])
+    .then(answers => {
+        let name = answers.name 
+        let cost = parseFloat(answers.cost)
+
+        connection.query(
+            'INSERT INTO departments SET ?',
+            {
+                department_name: name,
+                over_head_costs: cost
+            },
+            (err, res) => {
+                if (err) throw err
+                viewDepartments()
+                askSupervisor()
+            }
+        )
+    })
+}
+
+function viewDepartments() {
+    connection.query('SELECT * FROM departments', (err, results) => {
+        if (err) throw err 
+
+        let table = new Table({
+            head: ['id', 'department name', 'overhead costs']
+        })
+
+        results.forEach( item => {
+            table.push([item.department_id, item.department_name, item.over_head_costs])
+        })
+
+        console.log('\n' + table.toString())
+    })
 }
